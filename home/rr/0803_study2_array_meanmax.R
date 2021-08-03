0be1a70b51d903f7b5db1cbebc2a911f2e9825b4
kuick1kim <82426843+kuick1kim@users.noreply.github.com>
2021-08-03 15:12:31 +0900

    Add files via upload

---
+++ home/rr/0803_study2_array_meanmax.R
@@ -0,0 +1,5 @@
+x= c(1,2,3,100)
+x
+mean(x)
+max(x)
+min(x)
---
+++ home/rr/0803_study2_array_str.R
@@ -0,0 +1,12 @@
+str1="a"
+str1
+
+str2="text"
+
+str3="Hello world"
+
+str4= c("a","b","c")
+
+str5= c("hello!","world","is","good")
+
+str +2 #안됨
